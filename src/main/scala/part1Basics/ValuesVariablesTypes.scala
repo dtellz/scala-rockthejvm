ccb8ac6bca8b3228aa344196fdfd525ff529131c
dtellz <dtellez95@gmail.com>
2023-04-04 18:37:27 +0200

    part 1 basics

---
+++ src/main/scala/part1Basics/ValuesVariablesTypes.scala
@@ -0,0 +1,11 @@
+package part1Basics
+
+object ValuesVariablesTypes extends App {
+  val x: Int = 42;
+
+  println(x);
+
+  // VALS are inmutable! cant reassign x = 2
+
+  // COMPILER can infer types
+}
